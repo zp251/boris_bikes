4391844e88e7e04188ce1c7e9824dba63d38a9a3
zoe pot <pot.zoe.s@gmail.com>
2018-11-27 15:51:58 +0000

    Challenge 8

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,4 +1,7 @@
 require "boris_bike"
-describe DockingStation do
 
+describe DockingStation do
+  it "releases a bike from the docking station" do
+    expect(subject).to respond_to :release_bike
+  end
 end
