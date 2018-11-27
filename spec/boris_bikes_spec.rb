62c1cc5b2c027c245d304664bc4ffdcc9d9c4df1
zoe pot <pot.zoe.s@gmail.com>
2018-11-27 16:30:43 +0000

    Challenge 10: Making stations get bikes

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -4,4 +4,10 @@ describe DockingStation do
   it "releases a bike from the docking station" do
     expect(subject).to respond_to :release_bike
   end
+
+  it "gets working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
